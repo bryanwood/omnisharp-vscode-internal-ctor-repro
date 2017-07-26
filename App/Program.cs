8c213dfb199aea0b025d5c8ea9b012dcec18bf7d
Bryan Wood <bryan@bryanwood.com.au>
2017-07-26 15:31:34 +1000

    initial commit

---
+++ App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lib = new Lib.Class1();
+            var lib2 = new Lib.Class1("param");
+        }
+    }
+}
---
+++ Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Lib
+{
+    public class Class1
+    {
+        public Class1() { }
+
+        internal Class1(string param) { }
+    }
+}
